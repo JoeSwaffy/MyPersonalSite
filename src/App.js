2fcd712588f7ea91e6c87c8bc4735900f884393f
Joseph Swafford <swaffytech@gmail.com>
2023-03-03 16:45:35 -0600

    Adding router functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/Header'
 import BlogArea from './components/Blog'
 import SigninForm from './components/SigninForm';
-import { BrowserRouter, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 function App() {
   return (
     <>
     <Switch>
         <BrowserRouter>
+        <Route exact path="/">
+          {/*  Home Page Component here. */}
+        </Route>
         </BrowserRouter>
     </Switch>
       <Header />
